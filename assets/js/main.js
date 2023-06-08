fd7e29bec1dd79a05c1a452ace4c2e43c99a68b2
mjung8 <mmkjung@gmail.com>
2023-06-08 01:01:50 -0400

    Add template for user input

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,10 +23,14 @@ buildTableButton.addEventListener("click", () => {
     // 2, 2, 2, is a 3-leg 2-way devig
 
     // first need a datastructure to keep track of this stuff
-    // object:
-    // id, book, boostDescription, 
-    // LegsArray [e.g. 4 > 1; or 2, 2, 2 > 1, 1, 1; or 2, 3, 2 > 1, 1, 1] just placeholder...
-    // SidesArray[e.g. 4 > 4; or 2, 2, 2 > 2, 2, 2; or 2, 3, 2 > 2, 3, 2]
-    // 
+    // for each line, create an object:
+        // id, book, boostDescription, 
+        // inputsArray[e.g. 4 > 4; or 2, 2, 2 > 2, 2, 2; or 2, 3, 2 > 2, 3, 2]
+        // number of eventContainers = lines.Count;
+        // number of legGroupings = SidesArray.Count
+        // number of userInputs in each legGrouping is mapped by inputsArray
+        // for each i in inputsArray[]
+          // var splits = inputsArray[i]
+          // for each splits, create a leg grouping, and the value of the array for number of user inputs
 
 });
