1cfcb868527a899e10e7fdae3e170014b0cb6ec6
mjung8 <mmkjung@gmail.com>
2023-06-13 04:32:38 -0400

    Add test

--- assets/js/main.js
+++ assets/js/main.js
@@ -198,6 +198,8 @@ const CreateBoostsFromTextArea = () => {
 };
 
 const CollectUserInputsAndUpdateObjects = () => {
+    console.time("CollectUserInputsAndUpdateObjects");
+
     const eventContainers = document.getElementsByClassName("eventContainer");
     for (let i = 0; i < eventContainers.length; i++) {
         const eventContainer = eventContainers[i];
@@ -240,15 +242,19 @@ const CollectUserInputsAndUpdateObjects = () => {
             alert("Event doesn't exist for some reason...");
         }
     }
+    console.timeEnd("CollectUserInputsAndUpdateObjects");
+
 };
 
 // TODO create function for doing calculations
 const CalculateDeviggedOdds = () => {
+    console.time("CalculateDeviggedOdds");
+
     // get all python functions
 
     // loop globalBoosts to get deviggedOdds
     globalDeviggedBoosts = [];
-    
+
     const temp = globalBoosts[0];
     console.log(temp.odds);
 
@@ -334,6 +340,9 @@ const CalculateDeviggedOdds = () => {
 
     globalDeviggedBoosts.push(deviggedBoost);
     console.log(globalDeviggedBoosts);
+
+    console.timeEnd("CalculateDeviggedOdds");
+
 };
 
 //#endregion
@@ -391,6 +400,8 @@ testTextAreaButton.addEventListener("click", () => {
 const testOddsButton = document.getElementById("testOddsButton");
 testOddsButton.addEventListener("click", () => {
     console.log("testOddsButton clicked...");
+    console.time("testOddsButton");
+
     globalBoosts = [];
     const testBoosts = [
         {
@@ -482,6 +493,216 @@ testOddsButton.addEventListener("click", () => {
     testBoosts.forEach(testBoost => globalBoosts.push(Object.assign(new Boost, testBoost)));
     document.getElementById("allContainer").innerHTML = "";
     CreateHtmlFromBoosts(globalBoosts, document.getElementById("allContainer"));
+    
+    console.timeEnd("testOddsButton");
+});
+
+const testCalculationsButton = document.getElementById("testCalculationsButton");
+testCalculationsButton.addEventListener("click", () => {
+    console.log("testCalculationsButton clicked...");
+    console.time("testCalculationsButton");
+
+    globalDeviggedBoosts = [];
+    const temp = [
+        {
+            "betId": "061220231",
+            "originalOdds": [
+                [
+                    -408,
+                    530,
+                    1229
+                ],
+                [
+                    149,
+                    195,
+                    256
+                ],
+                [
+                    100,
+                    260,
+                    307
+                ]
+            ],
+            "originalBoosted": 650,
+            "decimalOdds": [
+                {
+                    "0": 1.2450980392156863,
+                    "1": 6.3,
+                    "2": 13.29
+                },
+                {
+                    "0": 2.49,
+                    "1": 2.95,
+                    "2": 3.56
+                },
+                {
+                    "0": 2,
+                    "1": 3.6,
+                    "2": 4.07
+                }
+            ],
+            "decimalBoosted": [
+                {
+                    "0": 7.5
+                }
+            ],
+            "multiplicative": [
+                {
+                    "0": 0.7744005214323721,
+                    "1": 0.15304834457191294,
+                    "2": 0.07255113399571494
+                },
+                {
+                    "0": 0.393158105563438,
+                    "1": 0.3318520958823596,
+                    "2": 0.27498979855420247
+                },
+                {
+                    "0": 0.48853027473993066,
+                    "1": 0.2714057081888504,
+                    "2": 0.240064017071219
+                }
+            ],
+            "multiAmerican": [
+                {
+                    "0": -343.2634358683724,
+                    "1": 553.3883151739216,
+                    "2": 1278.3382077240344
+                },
+                {
+                    "0": 154.35059988573605,
+                    "1": 201.33906412165513,
+                    "2": 263.6498536518957
+                },
+                {
+                    "0": 104.69560469560469,
+                    "1": 268.4520884520884,
+                    "2": 316.55555555555554
+                }
+            ],
+            "multiFV": 6.723194058805563,
+            "additive": [
+                {
+                    "0": 0.7907748363659208,
+                    "1": 0.1463553887968669,
+                    "2": 0.06286977483721232
+                },
+                {
+                    "0": 0.3944436413845568,
+                    "1": 0.3318202665292032,
+                    "2": 0.27373609208624
+                },
+                {
+                    "0": 0.4921739921739922,
+                    "1": 0.26995176995177,
+                    "2": 0.23787423787423786
+                }
+            ],
+            "addiAmerican": [
+                {
+                    "0": -377.95398155306657,
+                    "1": 583.2683157214963,
+                    "2": 1490.58944077545
+                },
+                {
+                    "0": 153.52164291198838,
+                    "1": 201.36796961194378,
+                    "2": 265.3153635600789
+                },
+                {
+                    "0": 103.18017934732362,
+                    "1": 270.4365413787291,
+                    "2": 320.3902065799541
+                }
+            ],
+            "addiFV": 6.513936775227642,
+            "power": [
+                {
+                    "0": 0.7928812134815131,
+                    "1": 0.14247517944539972,
+                    "2": 0.06464360707362952
+                },
+                {
+                    "0": 0.39436956894027564,
+                    "1": 0.3317517637610316,
+                    "2": 0.2738786672977336
+                },
+                {
+                    "0": 0.4921922082816954,
+                    "1": 0.2698149075903434,
+                    "2": 0.23799288412740044
+                }
+            ],
+            "powerAmerican": [
+                {
+                    "0": -382.8147252160258,
+                    "1": 601.8766383678966,
+                    "2": 1446.943379661647
+                },
+                {
+                    "0": 153.569260601708,
+                    "1": 201.43019849031546,
+                    "2": 265.1251884152407
+                },
+                {
+                    "0": 103.17265961830748,
+                    "1": 270.6244435975671,
+                    "2": 320.18063004971526
+                }
+            ],
+            "powerFV": 6.497611520858406,
+            "shin": [
+                {
+                    "0": 0.7866035761728085,
+                    "1": 0.14791855259839481,
+                    "2": 0.06547787122977276
+                },
+                {
+                    "0": 0.39411744166243246,
+                    "1": 0.3318274147179312,
+                    "2": 0.2740551436199598
+                },
+                {
+                    "0": 0.49125081791379815,
+                    "1": 0.27031815899278094,
+                    "2": 0.23843102309355182
+                }
+            ],
+            "shinAmerican": [
+                {
+                    "0": -368.6114144114244,
+                    "1": 576.0477184461388,
+                    "2": 1427.2335236599756
+                },
+                {
+                    "0": 153.73147551701484,
+                    "1": 201.36147757714556,
+                    "2": 264.89006803197566
+                },
+                {
+                    "0": 103.56200204341933,
+                    "1": 269.9344519532281,
+                    "2": 319.4085094403322
+                }
+            ],
+            "shinFV": 6.566215651977585,
+            "juice": {
+                "0": 1.0371243097998755,
+                "1": 1.0214883529547631,
+                "2": 1.0234780234780234
+            },
+            "testString": "-408/530/1229,149/195/256,100/260/307"
+        }
+    ];
+    CalculateDeviggedOdds();
+    //  TODO compare
+    assert(globalDeviggedBoosts[0].betId === temp[0].betId, "betId match");
+    assert(globalDeviggedBoosts[0].addiFV === temp[0].addiFV, "addiFV match");
+    assert(globalDeviggedBoosts[0].multiFV === temp[0].multiFV, "multiFV match");
+    assert(globalDeviggedBoosts[0].powerFV === temp[0].powerFV, "powerFV match");
+    assert(globalDeviggedBoosts[0].shinFV === temp[0].shinFV, "shinFV match");
+
+    console.timeEnd("testCalculationsButton");
 });
 
 const consoleLogBoost = (boost) => {
@@ -492,5 +713,14 @@ const consoleLogBoost = (boost) => {
     console.log("odds: " + boost.odds);
     console.log("boosted: " + boost.boosted);
 }
+
+const assert = (condition, message) => {
+    if (!condition) {
+        throw new Error(message || "Assertion failed");
+    } else {
+        console.log(message || "Assertion success");
+    }
+}
+
 
 //#endregion
